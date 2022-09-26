fd93969dd2722dcc7d2957b1f372ae8ba8de8b96
Kim-Siang Khaw's student <chencheng@bl-0.phy.sjtulocal>
2022-09-26 15:28:15 +0800

    update

--- initialize_inpac.sh
+++ initialize_inpac.sh
@@ -7,8 +7,8 @@ echo -e "initialize framework for Run-${run} analysis"
 source  /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
 
 mkdir ./run && mkdir ./build
-cd run && ln -s launch_fitter_run2.py run_scan.sh *.condor ../run
+cd OmegaFitting && ln -s launch_fitter_run2.py run_scan.sh *.condor ../run
 
-cd ../build && cmake ../OmegaFitting -DRun=${run} -DMain=Run2 -DUSE_JSON=OFF && cmake --build .
+cd ../build && cmake ../OmegaFitting -DRun=${run} -DMain=${main} -DUSE_JSON=OFF && cmake --build . && cd ../run
 
 
