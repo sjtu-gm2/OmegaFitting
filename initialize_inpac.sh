676a95ac3288d16648b3c5dafa1e1d711a26ae69
Kim-Siang Khaw's student <chencheng@bl-0.phy.sjtulocal>
2022-09-26 15:29:08 +0800

    update

--- initialize_inpac.sh
+++ initialize_inpac.sh
@@ -9,6 +9,6 @@ source  /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
 mkdir ./run && mkdir ./build
 cd OmegaFitting && ln -s launch_fitter_run2.py run_scan.sh *.condor ../run
 
-cd ../build && cmake ../OmegaFitting -DRun=${run} -DMain=${main} -DUSE_JSON=OFF && cmake --build . && cd ../run
+cd ../build && cmake ../OmegaFitting -DRun=${run} -DMain=${main} -DUSE_JSON=OFF && cmake --build . && cd ../run && mkdir logs
 
 
