ca1321d881f2b3c6cc9b7cb2ac579eafac9a30b1
Kim-Siang Khaw's student <chencheng@bl-0.phy.sjtulocal>
2022-09-26 15:34:32 +0800

    update

--- initialize_inpac.sh
+++ initialize_inpac.sh
@@ -7,7 +7,7 @@ echo -e "initialize framework for Run-${run} analysis"
 source  /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
 
 mkdir ./run && mkdir ./build
-cd OmegaFitting && ln -s launch_fitter_run2.py run_scan.sh *.condor ../run
+cd OmegaFitting && ln -s $PWD/launch_fitter_run2.py $PWD/run_scan.sh $PWD/*.condor ../run
 
 cd ../build && cmake ../OmegaFitting -DRun=${run} -DMain=${main} -DUSE_JSON=OFF && cmake --build . && cd ../run && mkdir logs
 
--- launch_fitter_run2.py
+++ launch_fitter_run2.py
@@ -11,47 +11,11 @@ default_config = {
     'lm_name' : 'run2all_integral',
 }
 
-def fit_random_type_calo(method,random_type,calo,time_offset):
-
-    wiggle_file = './data_random_type/wiggles_T.root'
-    wiggle_name = 'wiggle_T1650_calo{0:}_{1:}_{2:}'.format(calo,method,random_type)
-
-
-    lm_file = default_config['lm_file']
-    lm_name = default_config['lm_name']
-    
-    output_dir = './output/condor_{3:}_T1650_calo{0:}_{1:}_{2:}'.format(calo,method,random_type,job)
-    
-    output_val_condor = './values/condor_{3:}_T1650_calo{0:}_{1:}_{2:}'.format(calo,method,random_type,job)
-    os.system('mkdir -p %s'%(output_dir))    
-    os.system('mkdir -p %s'%(output_val_condor))
-
-    mode = 10
-
-    func_name = '13paras_cbo_vo'
-
-    output_file = '{0:}/calo_{1:}.root'.format(output_val_condor,calo)
-    output_name = 'T1650_calo{0:}_{1:}_{2:}_time{3:}_{4:}'.format(calo,method,random_type,time_offset,func_name)
-
-    initial_file = './values/condor_{3:}_T1650_calo{0:}_{1:}_{2:}/calo_{0:}.root'.format(21,method,random_type,job)
-    initial_name = 'T1650_calo{0:}_{1:}_{2:}_time{3:}_{4:}'.format(21,method,random_type,time_offset,func_name)
-
-    tag_out = 'T1650_calo{0:}_{1:}_{2:}_time{3:}'.format(calo,method,random_type,time_offset)
-
-    max_try = 5
-
-    os.system('../build/MAIN {0:} {1:} {2:} {3:} {4:} {5:} {6:} {7:} {8:} {9:} {10:}'.format(
-        wiggle_file,wiggle_name,lm_file,lm_name,initial_file,initial_name,output_dir,tag_out,mode,max_try,time_offset))
-
-    res_name = '{0:}/result_{1:}_{2:}.root'.format(output_dir,func_name,tag_out)
-    res_func_name = 'func_{0:}_{1:}'.format(func_name,tag_out)
-
-    trans.extract(res_name,res_func_name,output_file,output_name)
 
 
 def fit_random_type_seed_scan(method,random_type,seed):
 
-    wiggle_file = './wiggles/wiggles_%s.root'%(random_type)
+    wiggle_file = '/home/chencheng/Fitter_Wa_run2/run/wiggles/wiggles_%s.root'%(random_type)
     
     wiggle_name = 'wiggle_T1700_seed%s'%(seed)
     
@@ -92,7 +56,7 @@ def fit_random_type_seed_scan(method,random_type,seed):
     trans.extract(res_name,res_func_name,output_file,output_name)
 
 def fit_random_type_pileup_scan(method,random_type,scan_point):
-    wiggle_file = './wiggles/wiggles_%s.root'%(random_type)
+    wiggle_file = '/home/chencheng/Fitter_Wa_run2/run/wiggles/wiggles_%s.root'%(random_type)
 
     wiggle_name = 'wiggle_T1700_seed0_pu{0:.1f}'.format(scan_point*0.1)
     
