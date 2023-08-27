796557544df58b920ea6635ee452886f89fddd91
Zejia Lu <lzj777@sjtu.edu.cn>
2023-08-27 12:21:59 +0800

    add rebuild function

--- initialize_inpac.sh
+++ initialize_inpac.sh
@@ -1,6 +1,7 @@
 # setup root and c++
 
 main=Main
+first_build=${2:-true}
 
 if [[ ${1} == "run23" ]];then
     run=2.0
@@ -11,14 +12,20 @@ else
     exit 1
 fi
 
-echo -e "initialize framework for Run-${run} analysis"
-
 source  /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
 
-mkdir ./run && mkdir ./build
+if [[ ${first_build} == true ]]
+then
+    echo -e "initialize framework for Run-${run} analysis"
 
-cd OmegaFitting && ln -s $PWD/launch.py $PWD/example.json $PWD/configs ../run
+    mkdir ./run && mkdir ./build
+    cd OmegaFitting && ln -s $PWD/launch.py $PWD/example.json $PWD/configs ../run
+    cd ../build && cmake ../OmegaFitting -DRun=${run} -DMain=${main} -DUSE_JSON=OFF && cmake --build . && cd ../run && mkdir logs
+else
+    echo -e "rebuild framework for Run-${run} analysis"
+    mkdir -p build
+    cd build && cmake ../OmegaFitting -DRun=${run} -DMain=${main} -DUSE_JSON=OFF && cmake --build . && cd ..
+fi
 
-cd ../build && cmake ../OmegaFitting -DRun=${run} -DMain=${main} -DUSE_JSON=OFF && cmake --build . && cd ../run && mkdir logs
 
 
